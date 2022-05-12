1f43794ae2ee100e0dcaf73fcff38b34bb77e1a3
Olusolazainab <olusolazainab@gmail.com>
2022-05-12 22:47:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// import UseStateBasics from './component/UseStateBasics';
-// import UseStateArray from './component/UseStateArray';
-// import UseStateObject from './component/UseStateObject';
-// import UseStateCounter from './component/UseStateCounter';
-// import UseEffectBasics from './component/UseEffectBasics';
-// import UseeffectClearnup from './component/UseeffectClearnup';
-// import MultipleReturns from './component/MultipleReturns';
-// import One from './component/IndiaGuy/One';
-// import Sample from './component/IndiaGuy/Sample';
-// import Samp from './component/IndiaGuy/Samp';
-// import BlogList from './component/NINJA/BlogList';
+import BlogList from './component/NINJA/BlogList';
 import Home from './component/NINJA/Home';
 import Navbar from './component/NINJA/Navbar';
 import Create from './component/NINJA/Create';
 import BlogDetails from './component/NINJA/BlogDetails';
-import NotFound from './component/NINJA/NotFound';
-
-
-
+import NotFound from './component/NINJA/NotFound'
 
 function App() {
 
   return (
     <Router>
     <div className='App'>
-      {/* <UseStateBasics />
-      <UseStateArray />
-      <UseStateObject />
-      <UseStateCounter />
-      <UseEffectBasics />
-      <UseeffectClearnup />
-      <MultipleReturns /> */}
-      {/* <One />
-      <Sample />
-      <Samp /> */}
        <Navbar />
         < div className='content'>
           <Switch>
